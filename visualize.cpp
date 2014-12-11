2362e7894145146c7a2967b14deaf4ea4303f84e
Georgy Borisenko <gosha-bor@yandex.ru>
2014-12-12 02:25:43 +0300

    Started vizualization development

---
+++ visualize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  Gnuplot g;
+  g.showonscreen();
+  int k = 1;
+  while( k < argc )
+  {
+
+  }
+}
