19ade16d9ba65361c977743ae18fbfe9a5e29578
Georgy Borisenko <gosha-bor@yandex.ru>
2014-12-23 12:59:20 +0300

    First dirction of distance field

--- send.js
+++ send.js
@@ -79,7 +79,82 @@ function PrepareImage()
   }
   ctx.putImageData(simg, 0, 0);
 
+  //Distance field
+  var d = []; 
+  for(var i = 0; i < RecResolution; ++i)
+  {
+    d[i] = [];
+    for(var j = 0; j < RecResolution; ++j)
+    {
+      var p = i*RecResolution*4 + j*4;
+      if(simg.data[p+3] > 200  ) //plotted pixel
+      {
+        d[i][j] = {x: 0, y: 0, c: 0};
+      }
+      else
+      {
+        d[i][j] = {x: RecResolution, y: RecResolution, c: 255};
+      }
+    }
+  }
+  checkPixel = function(y, x, vy, vx)
+  {
+    if(y+vy<0 || x+vx<0 || y+vy >= RecResolution || x+vx >= RecResolution)
+      return RecResolution*RecResolution;
+    var cp = d[y+vy][x+vx];
+    return (true ? (cp.x - vx)*(cp.x - vx) + (cp.y - vy)*(cp.y - vy) : RecResolution*RecResolution);
+  }
+  for(var j = 1; j + 1 < RecResolution; ++j)
+  {
+    for(var i = 1; i + 1 < RecResolution; ++i)
+    {
+      var min = checkPixel(j, i, 0, 0);
+      console.log("min", min);
+      var t;
+      if((t = checkPixel(j, i, -1, 0)) < min)
+      {
+        min = t;
+        d[j][i].x = d[j-1][i].x ;
+        d[j][i].y = d[j-1][i].y + 1;
+      }
+      if((t = checkPixel(j, i, -1, -1)) < min)
+      {
+        min = t;
+        d[j][i].x = d[j-1][i-1].x + 1;
+        d[j][i].y = d[j-1][i-1].y + 1;
+      }
+      if((t = checkPixel(j, i, 0, -1)) < min)
+      {
+        min = t;
+        d[j][i].x = d[j][i-1].x + 1;
+        d[j][i].y = d[j][i-1].y;
+      }
 
+      if((t = checkPixel(j, i, -1, +1)) < min)
+      {
+        min = t;
+        d[j][i].x = d[j-1][i+1].x - 1;
+        d[j][i].y = d[j-1][i+1].y + 1;
+      }
+    }
+  }
+  var cov = 50;
+  var dimg = ctx.createImageData(RecResolution, RecResolution);
+  for(var i = 0; i < RecResolution; ++i)
+  {
+    for(var j = 0; j < RecResolution; ++j)
+    {
+      var p = i*RecResolution*4 + j*4;
+      var c = 255 - 255*(cov/(cov+Math.sqrt((d[i][j].x)*(d[i][j].x) + d[i][j].y*d[i][j].y)));
+      console.log((d[i][j].x)*(d[i][j].x) + d[i][j].y*d[i][j].y);
+      c = Math.floor(c);
+      dimg.data[p + 0] = c;
+      dimg.data[p + 1] = c;
+      dimg.data[p + 2] = c;
+      dimg.data[p + 3] = 255;
+    }
+  }
+  ctx.putImageData(dimg, 500, 0);
 
 }
 
